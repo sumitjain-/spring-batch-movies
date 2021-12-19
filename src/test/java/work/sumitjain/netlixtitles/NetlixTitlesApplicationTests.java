3cabf4e69895affb52a627d36e89ec4cab094a9f
Sumit Jain <sumeetsuniljain@gmail.com>
2021-12-19 11:55:03 +0530

    Init spring starter

---
+++ src/test/java/work/sumitjain/netlixtitles/NetlixTitlesApplicationTests.java
@@ -0,0 +1,13 @@
+package work.sumitjain.netlixtitles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetlixTitlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
